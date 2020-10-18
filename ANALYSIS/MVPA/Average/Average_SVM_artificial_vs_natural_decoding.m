583b4082edaca072e440017368a0e06c18c924e9
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-10-18 19:32:54 +0200

    added code for plotting all subjects on one plot

--- ANALYSIS/MVPA/Average/Average_SVM_artificial_vs_natural_decoding.m
+++ ANALYSIS/MVPA/Average/Average_SVM_artificial_vs_natural_decoding.m
@@ -7,23 +7,46 @@ function Average_SVM_artificial_vs_natural_decoding(subjects)
 %Returns a plot of the average category decoding over timepoints. 
 %
 
-
 %Add paths
 addpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS/OTHER');
-addpath(genpath('/home/agnek95/SMST/PDM/ANALYSIS/'));
+% addpath(genpath('/home/agnek95/SMST/PDM/ANALYSIS/'));
 results_dir = '/home/agnek95/SMST/PDM_PILOT_2/RESULTS/';
 addpath(genpath(results_dir));
 
 %Define variables
 numTimepoints = 200;
 allSubjects = NaN(numel(subjects),numTimepoints);
+legend_cell = cell(numel(subjects) + 1,1);
+
+%Prepare figure
+figure(abs(round(randn*10)));
+set(gcf, 'Position', get(0, 'Screensize')); %make fullscreen
 
 % Loop: create a matrix containing the decoding accuracy matrices of each subject
 for subject = subjects
     subname = get_subject_name(subject);
-    load(fullfile(results_dir,subname,'svm_artificial_vs_natural_decoding_accuracy.mat'));
-    allSubjects(subject,:) = decodingAccuracy_Avg; 
+    load(fullfile(results_dir,subname,'pseudotrials_svm_artificial_vs_natural_decoding_accuracy.mat'));
+    plot(decodingAccuracy_avg,'LineWidth',2);
+    hold on;
+    legend_cell{subject} = sprintf('Subject %d',subject);
+    allSubjects(subject,:) = decodingAccuracy_avg; 
 end
 
+avg_decoding_accuracy = squeeze(mean(allSubjects,1)); % avg over subjects
+
+%Plot
+plot(avg_decoding_accuracy,'--','LineWidth',2);
+title(sprintf('Decoding of artificial vs natural scenes per timepoint (N=%d)',numel(subjects)));
+legend_cell{end} = sprintf('Average of %d subjects',numel(subjects));
+legend(legend_cell);
+xlabel('Timepoint');
+ylabel('Decoding accuracy (%)');
+leg = legend(legend_cell,'FontSize',12);
+set(leg, 'position', [0.7 0.3 0.1 0.01]); %put legend below graph
+
+%Save
+results_avg = '/home/agnek95/SMST/PDM_PILOT_2/RESULTS_AVG/';
+saveas(gcf,fullfile(results_avg,sprintf('svm_artificial_vs_natural_decoding_accuracy_%d_subjects',numel(subjects))));
+saveas(gcf,fullfile(results_avg,sprintf('svm_artificial_vs_natural_decoding_accuracy_%d_subjects.svg',numel(subjects))));
 
 end
