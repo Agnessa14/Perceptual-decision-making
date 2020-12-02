76a9c0594f550f5b18c63218116902529d1f02e2
agnessakarapetian@gmail.com <agnessakarapetian@gmail.com>
2020-12-02 12:52:25 +0100

    initial commit

---
+++ ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
@@ -0,0 +1,11 @@
+function plotting_parameters(plot_title,plot_legend,onset_time)
+%PLOT_DATA Plot input data.
+%
+%Input: title of the plot, legend of the plot, stimulus onset (in seconds)
+%
+xlim([0 201]);
+xlabel('Timepoints');
+ylabel('Decoding accuracy');   
+title(plot_title);
+xline(onset_time,'--');
+legend(plot_legend);
