c319647ff35a358919334833bf975a8f3206624f
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-12-04 16:38:31 +0100

    included an if statement for conditions with 0 trials

---
+++ ANALYSIS_Full_experiment/OTHER/create_data_matrix.m
@@ -0,0 +1,27 @@
+function [dataMatrix] = create_data_matrix(numConditions,triggers,minNumTrials,data)
+%CREATE_DATA_MATRIX Construct the matrix of data containing voltage
+%information for each condition, trial, electrode and timepoint. 
+%
+%Input:  
+%   -numConditions: number of conditions (integer)
+%   -triggers: list of triggers as they appear in the experiment (Tx1,
+%   where T is the number of overall trials)
+%   -minNumTrials: the overall minimum number of trials as obtained by the
+%   min_number_trials function
+%   -data: TxExTP matrix containing the EEG data
+%
+%Output: NxMxExTP matrix containing the data in the desired format.
+%
+numElectrodes = size(data,2);
+numTimepoints = size(data,3);
+dataMatrix = NaN(numConditions,minNumTrials,numElectrodes,numTimepoints);
+for c = 1:numConditions
+    trialsMat = find(triggers==c);
+    trialsMat_randomized = trialsMat(randperm(numel(trialsMat))); %randomize
+    if numel(trialsMat_randomized)<minNumTrials
+        continue;
+    else
+        trialsMat_minimum = trialsMat_randomized(1:minNumTrials);
+        dataMatrix(c,:,:,:) = data(trialsMat_minimum,:,:); 
+    end
+end
