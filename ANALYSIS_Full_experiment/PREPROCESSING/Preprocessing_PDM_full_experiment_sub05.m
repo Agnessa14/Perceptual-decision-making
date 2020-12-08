ba718a380f49d0ff0739478b2cef98b09e649a5f
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-12-08 13:10:10 +0100

    compared triggers for the blocks 1-17

--- ANALYSIS_Full_experiment/PREPROCESSING/Preprocessing_PDM_full_experiment_sub05.m
+++ ANALYSIS_Full_experiment/PREPROCESSING/Preprocessing_PDM_full_experiment_sub05.m
@@ -26,97 +26,107 @@ addpath('/home/agnek95/OR/TOOLBOX/fieldtrip-20190224');
 ft_defaults;
 
 %%  Load EEG and behavioural data
-mainDirectoryEEG_1 = dir(fullfile(data_path,subname,'/EEG/','*.eeg')); 
-fileNameEEG_1 = fullfile(data_path, subname,'/EEG/', mainDirectoryEEG_1.name); 
-mainDirectoryBeh_1 = dir(fullfile(data_path,subname,'/Behavioural/','*.mat')); 
+mainDirectoryEEG = dir(fullfile(data_path,subname,'/EEG/','*.eeg')); 
+fileNameEEG = fullfile(data_path, subname,'/EEG/', mainDirectoryEEG.name); 
+mainDirectoryBeh = dir(fullfile(data_path,subname,'/Behavioural/','*.mat')); 
 
 %% Fix order of blocks since they do not load in the correct order
-filenamesAll = extractfield(mainDirectoryBeh_1,'name');
-for m = 1:numel(mainDirectoryBeh_1)  
+filenamesAll = extractfield(mainDirectoryBeh,'name');
+for m = 1:numel(mainDirectoryBeh)  
     parsedName = split(filenamesAll{m},'_');
     parsedBlocknum = split(parsedName{4},'.');
-    mainDirectoryBeh_1(m).blockNum = str2double(parsedBlocknum{1});     
+    mainDirectoryBeh(m).blockNum = str2double(parsedBlocknum{1});     
 end
 
 %reorder by block number
-T = struct2table(mainDirectoryBeh_1); 
+T = struct2table(mainDirectoryBeh); 
 sortedT = sortrows(T, 'blockNum'); 
-mainDirectoryBeh_1 = table2struct(sortedT); 
-filenamesSorted = extractfield(mainDirectoryBeh_1,'name');
+mainDirectoryBeh = table2struct(sortedT); 
+filenamesSorted = extractfield(mainDirectoryBeh,'name');
 
 %double check that the order is correct
-blocks = 1:numel(mainDirectoryBeh_1);
-blockorderarray = arrayfun(@(x) mainDirectoryBeh_1(x).blockNum,blocks);
+blocks = 1:numel(mainDirectoryBeh);
+blockorderarray = arrayfun(@(x) mainDirectoryBeh(x).blockNum,blocks);
 if ~isequal(blockorderarray,blocks)
     error('Incorrect block order: Check and fix manually');
 end
+
 %% Take only the 18 blocks
+filenamesSorted_1_17 = filenamesSorted(1:17);
 
 %% Take the task-related trials only
 %Behavioural data
-behav.triggers = [];
-behav.RT = [];
-behav.points = [];
+behav_1_17.triggers = [];
+behav_1_17.RT = [];
+behav_1_17.points = [];
 
-for block = 1:numel(filenamesSorted)
-    block_name = filenamesSorted{block};
+for block = 1:numel(filenamesSorted_1_17)
+    block_name = filenamesSorted_1_17{block};
     if contains(block_name,task_name)
         load(block_name);
-        behav.triggers = [behav.triggers;data.triggers];
-        behav.RT = [behav.RT; data.rt]; 
-        behav.points = [behav.points; data.points]; 
+        behav_1_17.triggers = [behav_1_17.triggers;data.triggers];
+        behav_1_17.RT = [behav_1_17.RT; data.rt]; 
+        behav_1_17.points = [behav_1_17.points; data.points]; 
     end
 end
 
 %EEG data
 %Define Events
-cfg=[];
-cfg.dataset=fileNameEEG_1;
-cfg.trialdef.eventtype='Stimulus';
-cfg.trialdef.prestim=0.2;
-cfg.trialdef.poststim=0.8;
-cfg=ft_definetrial(cfg);
+cfg_1_17=[];
+cfg_1_17.dataset=fileNameEEG;
+cfg_1_17.trialdef.eventtype='Stimulus';
+cfg_1_17.trialdef.prestim=0.2;
+cfg_1_17.trialdef.poststim=0.8;
+cfg_1_17=ft_definetrial(cfg_1_17);
 
 %Define the columns of cfg
-eegtriggers = cfg.trl(:,4);
-beginningEpoch = cfg.trl(:,1); %beginning of each trial relative to the beginning of the raw data
-endEpoch = cfg.trl(:,2); %end of each trial; column 2-column 1 = duration of epoch
-offsetTrigger = cfg.trl(:,3); %offset of the trigger with respect to the trial - defined by cfg.trialdef.prestim
+eegtriggers_1_17 = cfg_1_17.trl(:,4);
+beginningEpoch_1_17 = cfg_1_17.trl(:,1); %beginning of each trial relative to the beginning of the raw data
+endEpoch_1_17 = cfg_1_17.trl(:,2); %end of each trial; column 2-column 1 = duration of epoch
+offsetTrigger_1_17 = cfg_1_17.trl(:,3); %offset of the trigger with respect to the trial - defined by cfg.trialdef.prestim
 
 numConditions = 60;
-task_triggers = (1:numConditions)+task*100; 
-triggers_eeg = [];
-trials_remaining = [];
-for t = 1:numel(eegtriggers)
-    if ismember(eegtriggers(t),task_triggers)
-        trials_remaining = [trials_remaining;t];
-        triggers_eeg = [triggers_eeg;eegtriggers(t)];
+if task == 1
+    task_triggers_1_17 = (1:numConditions); 
+elseif task == 2
+    task_triggers_1_17 = (1:numConditions)+100;
+end
+
+triggers_eeg_1_17 = [];
+trials_remaining_1_17 = [];
+for t = 1:numel(eegtriggers_1_17)
+    if ismember(eegtriggers_1_17(t),task_triggers_1_17)
+        trials_remaining_1_17 = [trials_remaining_1_17;t];
+        triggers_eeg_1_17 = [triggers_eeg_1_17;eegtriggers_1_17(t)];
     end
 end
-triggers_eeg = triggers_eeg-task*100;
+
+if task == 2
+    triggers_eeg_1_17 = triggers_eeg_1_17-100;
+end
 
 %Remove the same trials from the other columns of cfg
-beginningEpoch = beginningEpoch(trials_remaining);
-endEpoch = endEpoch(trials_remaining);
-offsetTrigger = offsetTrigger(trials_remaining);
+beginningEpoch_1_17 = beginningEpoch_1_17(trials_remaining_1_17);
+endEpoch_1_17 = endEpoch_1_17(trials_remaining_1_17);
+offsetTrigger_1_17 = offsetTrigger_1_17(trials_remaining_1_17);
 
 %% Remove extra triggers
 %remove  paperclips from behavioural data
-paperclips = behav.triggers==999;
-behav.triggers = behav.triggers(~paperclips);
-behav.RT = behav.RT(~paperclips);
-behav.points = behav.points(~paperclips);
+paperclips = behav_1_17.triggers==999;
+behav_1_17.triggers = behav_1_17.triggers(~paperclips);
+behav_1_17.RT = behav_1_17.RT(~paperclips);
+behav_1_17.points = behav_1_17.points(~paperclips);
 
-for i = 1:numel(behav.triggers)
-    while round(triggers_eeg(i)) ~= round(behav.triggers(i))
-        triggers_eeg(i) = [];
-        beginningEpoch(i)=[];
-        endEpoch(i)=[];
-        offsetTrigger(i)=[];
+for i = 1:numel(behav_1_17.triggers)
+    while round(triggers_eeg_1_17(i)) ~= round(behav_1_17.triggers(i))
+        triggers_eeg_1_17(i) = [];
+        beginningEpoch_1_17(i)=[];
+        endEpoch_1_17(i)=[];
+        offsetTrigger_1_17(i)=[];
     end
 end
 
-if isequal(round(triggers_eeg),round(behav.triggers)) 
+if isequal(round(triggers_eeg_1_17),round(behav_1_17.triggers)) 
     disp('all good!');
 else
     warning('problem with the triggers: check manually');
@@ -124,58 +134,58 @@ else
 end
 
 %put back into the configuration file
-cfg.trl = [beginningEpoch endEpoch offsetTrigger triggers_eeg];
+cfg_1_17.trl = [beginningEpoch_1_17 endEpoch_1_17 offsetTrigger_1_17 triggers_eeg_1_17];
 
 %% Rest of preprocessing
 
 %Filter and read
-cfg.dataset=fileNameEEG_1;
-cfg.lpfilter = 'yes';
-cfg.lpfreq = 50;
-cfg.demean='yes'; %apply baseline correction
-cfg.baselinewindow=[-0.2,0];
-data=ft_preprocessing(cfg);
+cfg_1_17.dataset=fileNameEEG;
+cfg_1_17.lpfilter = 'yes';
+cfg_1_17.lpfreq = 50;
+cfg_1_17.demean='yes'; %apply baseline correction
+cfg_1_17.baselinewindow=[-0.2,0];
+data=ft_preprocessing(cfg_1_17);
 
 %Resample data to 200hz
-cfg=[];
-cfg.resamplefs=200;
-data=ft_resampledata(cfg,data);
+cfg_1_17=[];
+cfg_1_17.resamplefs=200;
+data=ft_resampledata(cfg_1_17,data);
 
 % Get rid of jumps
 %The data are preprocessed (again) with the following configuration parameters,
 %which are optimal for identifying jump artifacts.
-cfg.artfctdef.jump.medianfilter  = 'yes';
-cfg.artfctdef.jump.medianfiltord = 9;
-cfg.artfctdef.jump.absdiff       = 'yes';
+cfg_1_17.artfctdef.jump.medianfilter  = 'yes';
+cfg_1_17.artfctdef.jump.medianfiltord = 9;
+cfg_1_17.artfctdef.jump.absdiff       = 'yes';
 
 %  Artifacts are identified by means of thresholding the z-transformed value
 %  of the preprocessed data.
-cfg.artfctdef.jump.channel       = {'eeg'}; %Nx1 cell-array with selection of channels, see FT_CHANNELSEL
-cfg.artfctdef.jump.cutoff        = 20; %z-value at which to threshold (default = 20)
-[cfg, ~] = ft_artifact_jump(cfg, data);
+cfg_1_17.artfctdef.jump.channel       = {'eeg'}; %Nx1 cell-array with selection of channels, see FT_CHANNELSEL
+cfg_1_17.artfctdef.jump.cutoff        = 20; %z-value at which to threshold (default = 20)
+[cfg_1_17, ~] = ft_artifact_jump(cfg_1_17, data);
 
 %Remove the found artifacts
-cfg.feedback = 'yes';
-data = ft_rejectartifact(cfg, data);
+cfg_1_17.feedback = 'yes';
+data = ft_rejectartifact(cfg_1_17, data);
 
 % Display Summary Stats and exclude epochs/channels
-cfg=[];
-cfg.showlabel='yes';
-cfg.method='summary';
+cfg_1_17=[];
+cfg_1_17.showlabel='yes';
+cfg_1_17.method='summary';
 %cfg.layout='easycapM1.lay'; %not the one we used
-data=ft_rejectvisual(cfg,data);
+data=ft_rejectvisual(cfg_1_17,data);
 
 %Update the behavioural triggers, RT and points and save them
 for i = 1:numel(data.trialinfo)
-    while round(behav.triggers(i)) ~= round(data.trialinfo(i))
-        behav.triggers(i) = [];
-        behav.RT(i) = [];
-        behav.points(i) = [];
+    while round(behav_1_17.triggers(i)) ~= round(data.trialinfo(i))
+        behav_1_17.triggers(i) = [];
+        behav_1_17.RT(i) = [];
+        behav_1_17.points(i) = [];
     end
 end
 
 %Double-check that eeg and behavioural triggers are equal
-if isequal(round(data.trialinfo),round(behav.triggers)) 
+if isequal(round(data.trialinfo),round(behav_1_17.triggers)) 
     disp('all good!');
 else
     warning('problem with the triggers: check manually');
@@ -185,6 +195,6 @@ task_name_small = get_task_name(task);
 save(sprintf('%s/%s/preprocessed_behavioural_data_%s',data_path, subname, task_name_small),'behav');
 
 %Transform to "timelocked" data and save the output
-cfg.outputfile= sprintf('%s/%s/timelock_%s',data_path,subname,task_name_small); 
-cfg.keeptrials='yes';
-data=ft_timelockanalysis(cfg,data);
+cfg_1_17.outputfile= sprintf('%s/%s/timelock_%s',data_path,subname,task_name_small); 
+cfg_1_17.keeptrials='yes';
+data=ft_timelockanalysis(cfg_1_17,data);
