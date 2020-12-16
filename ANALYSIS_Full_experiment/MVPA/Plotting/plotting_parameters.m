99efdba24491a0922f2138e49ef267656c3462c9
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-12-16 19:44:34 +0100

    added legend location as input

--- ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
+++ ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
@@ -1,4 +1,4 @@
-function plotting_parameters(plot_title,plot_legend,onset_time,font_size)
+function plotting_parameters(plot_title,plot_legend,onset_time,font_size,legend_location)
 %PLOT_DATA Plot input data.
 %
 %Input: title of the plot, legend of the plot, stimulus onset (in seconds)
@@ -8,4 +8,4 @@ xlabel('Timepoints');
 ylabel('Decoding accuracy');   
 title(plot_title);
 xline(onset_time,'--');
-legend(plot_legend,'FontSize',font_size);
+legend(plot_legend,'Location',legend_location,'FontSize',font_size);
