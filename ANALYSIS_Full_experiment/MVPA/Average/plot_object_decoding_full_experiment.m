09b5fb1cac016bb3030a3cb34948ed5c4f3f1ba0
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-12-16 19:12:17 +0100

    added font size to legend

--- ANALYSIS_Full_experiment/MVPA/Average/plot_object_decoding_full_experiment.m
+++ ANALYSIS_Full_experiment/MVPA/Average/plot_object_decoding_full_experiment.m
@@ -29,22 +29,17 @@ for subject = subjects
     subname = get_subject_name(subject);
     subject_results_dir = fullfile(results_dir,subname);
     load(fullfile(subject_results_dir,sprintf('svm_decoding_accuracy_%s.mat',task_name)));
-    
-    %matrix of all subjects
     decoding_accuracies_all_subjects(subject,:,:,:) = decodingAccuracy_avg;
    
-    %plot the object decoding curve for the participant
-    avg_over_conditions = squeeze(nanmean(decodingAccuracy_avg,[1,2]));
+    %plot the object decoding curve for the participant    
+    avg_over_conditions = squeeze(nanmean(nanmean(decodingAccuracy_avg,1),2));
     plot(avg_over_conditions, 'Linewidth',2);
     hold on;
-    
-    %legend
     legend_cell{subject} = sprintf('Subject %d',subject);   
 end   
 
 %% Remove any NaN (for non-included subjects)
-avg_over_conditions_all_subjects = squeeze(nanmean(decoding_accuracies_all_subjects,1:3));
-% decoding_accuracies_all_subjects(isnan(decoding_accuracies_all_subjects(1,1,1,1)),:,:,:) = []; %if a subject's first datapoint is nan, assume their entire data is nans
+avg_over_conditions_all_subjects = squeeze(nanmean(nanmean(nanmean(decoding_accuracies_all_subjects,1),2),3));
 legend_cell(cellfun(@(x) any(isnan(x)),legend_cell)) = [];
 
 %% Plot the average of all subjects
@@ -54,7 +49,7 @@ title = sprintf('Object decoding per timepoint for %d subjects',numel(subjects))
 onset_time = 40; 
 legend_cell{numel(subjects)+1} = 'Average over all subjects';
 legend_cell{numel(subjects)+2} = 'Stimulus onset'; %last legend element
-plotting_parameters(title,legend_cell,onset_time);
+plotting_parameters(title,legend_cell,onset_time,10);
 
 %save the plot
 saveas(gcf,fullfile(results_avg_dir,sprintf('svm_object_decoding_%d_subjects_%s',numel(subjects),task_name))); %save as matlab figure
