e11527f80fcafb22b5a12cba12aa61354b57d8b1
Agnessa Karapetian <agnessakarapetian@gmail.com>
2020-12-17 14:21:48 +0100

    added y label as input param

--- ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
+++ ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
@@ -1,11 +1,11 @@
-function plotting_parameters(plot_title,plot_legend,onset_time,font_size,legend_location)
+function plotting_parameters(plot_title,plot_legend,onset_time,font_size,legend_location,label_y)
 %PLOT_DATA Plot input data.
 %
 %Input: title of the plot, legend of the plot, stimulus onset (in seconds)
 %
 xlim([0 201]);
 xlabel('Timepoints');
-ylabel('Decoding accuracy');   
+ylabel(label_y);   
 title(plot_title);
 xline(onset_time,'--');
 legend(plot_legend,'Location',legend_location,'FontSize',font_size);
