0a48a3e0f099302dc230c2aeeb391b3fa32a809c
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-04-09 17:25:40 +0200

    add category decoding code

--- ANALYSIS_Full_experiment/MVPA/Average/plot_object_decoding_full_experiment.m
+++ ANALYSIS_Full_experiment/MVPA/Average/plot_object_decoding_full_experiment.m
@@ -1,8 +1,9 @@
-function plot_object_decoding_full_experiment(subjects,task)
+function plot_object_decoding_full_experiment(subjects,task,analysis)
 %PLOT_OBJECT_DECODING_FULL_EXPERIMENT Plot the results from object decoding, averaged over
 %all participants and subject-specific.
 %
-%Input: subject IDs, task (1=categorization,2=distraction)
+%Input: subject IDs, task (1=categorization,2=distraction), analysis
+%('object_decoding' or 'category_decoding')
 %
 %Output: curve of decoding accuracies per timepoint
 
@@ -20,27 +21,46 @@ numTimepoints = 200;
 figure(abs(round(randn*10))); %Random figure number
 set(gcf, 'Position', get(0, 'Screensize'));
 sorted_subjects = sort(subjects); %order by ID
-decoding_accuracies_all_subjects = NaN(sorted_subjects(end),numConditions,numConditions,numTimepoints);
+if strcmp(analysis,'object_decoding')
+    decoding_accuracies_all_subjects = NaN(sorted_subjects(end),numConditions,numConditions,numTimepoints);
+elseif strcmp(analysis,'category_decoding')
+    decoding_accuracies_all_subjects = NaN(sorted_subjects(end),numTimepoints);
+end
+   
 legend_cell = cell(1,sorted_subjects(end));
 legend_cell(:) = {NaN};
 
 cmap = jet(max(subjects));
+
 %% Loop: collect results from all subjects + plot each subject individually on the same plot
 for subject = subjects
     subname = get_subject_name(subject);
     subject_results_dir = fullfile(results_dir,subname);
-    load(fullfile(subject_results_dir,sprintf('svm_decoding_accuracy_%s.mat',task_name)));
-    decoding_accuracies_all_subjects(subject,:,:,:) = decodingAccuracy_avg;
+    if strcmp(analysis,'object_decoding')
+        load(fullfile(subject_results_dir,sprintf('svm_decoding_accuracy_%s.mat',task_name)));
+        decoding_accuracies_all_subjects(subject,:,:,:) = decodingAccuracy_avg;
+
+    elseif strcmp(analysis,'category_decoding')
+        load(fullfile(subject_results_dir,sprintf('svm_artificial_vs_natural_decoding_accuracy_%s.mat',task_name)));
+        decoding_accuracies_all_subjects(subject,:) = decodingAccuracy_avg;
+    end
    
     %plot the object decoding curve for the participant    
-    avg_over_conditions = squeeze(nanmean(nanmean(decodingAccuracy_avg,1),2));
-    plot(avg_over_conditions, 'Linewidth',2, 'Color', cmap(subject, :));
+    if strcmp(analysis,'object_decoding')
+        decodingAccuracy_avg = squeeze(nanmean(nanmean(decodingAccuracy_avg,1),2));
+    end
+    plot(decodingAccuracy_avg, 'Linewidth',2, 'Color', cmap(subject, :));
     hold on;
     legend_cell{subject} = sprintf('Subject %d',subject);   
 end   
 
-%% Remove any NaN (for non-included subjects)
-avg_over_conditions_all_subjects = squeeze(nanmean(nanmean(nanmean(decoding_accuracies_all_subjects,1),2),3));
+%% Remove any NaN (for non-included subjects and conditions)
+if strcmp(analysis,'object_decoding')
+    avg_over_conditions_all_subjects = squeeze(nanmean(nanmean(nanmean(decoding_accuracies_all_subjects,1),2),3));
+elseif strcmp(analysis,'category_decoding')
+    avg_over_conditions_all_subjects = squeeze(nanmean(decoding_accuracies_all_subjects,1));
+end
+
 legend_cell(cellfun(@(x) any(isnan(x)),legend_cell)) = [];
 
 %% Plot the average of all subjects
@@ -51,16 +71,27 @@ if task==1
 elseif task==2
     task_title = 'distraction';
 end
-title = sprintf('Object decoding per timepoint for %d subjects in a %s task',numel(subjects),task_title);
+
+if strcmp(analysis,'object_decoding')
+    analysis_title = 'Object';
+elseif strcmp(analysis,'category_decoding')
+    analysis_title = 'Category';
+end
+
+title = sprintf('%s decoding per timepoint for %d subjects in a %s task',analysis_title,numel(subjects),task_title);
 onset_time = 40; 
 legend_cell{numel(subjects)+1} = 'Average over all subjects';
 legend_cell{numel(subjects)+2} = 'Stimulus onset'; %last legend element
 xticks(0:10:200);
-plotting_parameters(title,legend_cell,onset_time,8,'best','Decoding accuracy (%)'); %'best' location or specify position [0.75 0.7 0.1 0.1]
+plotting_parameters(title,legend_cell,onset_time,8,'best','Decoding accuracy (%)'); %'best' location or specify position [0.75 0.7 0.1 0.1]  
 
-%save the plot
-saveas(gcf,fullfile(results_avg_dir,sprintf('svm_object_decoding_%d_subjects_%s',numel(subjects),task_name))); %save as matlab figure
-saveas(gcf,fullfile(results_avg_dir,sprintf('svm_object_decoding_%d_subjects_%s.svg',numel(subjects),task_name))); %save as svg
-close(gcf);    
-
+%% Save the plot
+if strcmp(analysis,'object_decoding')
+    saveas(gcf,fullfile(results_avg_dir,sprintf('svm_object_decoding_%d_subjects_%s',numel(subjects),task_name))); %save as matlab figure
+    saveas(gcf,fullfile(results_avg_dir,sprintf('svm_object_decoding_%d_subjects_%s.svg',numel(subjects),task_name))); %save as svg
+elseif strcmp(analysis,'category_decoding')
+    saveas(gcf,fullfile(results_avg_dir,sprintf('svm_artificial_vs_natural_%d_subjects_%s',numel(subjects),task_name))); %save as matlab figure
+    saveas(gcf,fullfile(results_avg_dir,sprintf('svm_artificial_vs_natural_%d_subjects_%s.svg',numel(subjects),task_name))); %save as svg    
+end
+close(gcf)
 end
