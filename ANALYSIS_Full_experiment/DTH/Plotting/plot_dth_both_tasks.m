5570b4c5a88396c5822c072e42807a663f31c4df
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-05-04 16:59:06 +0200

    added stats

--- ANALYSIS_Full_experiment/DTH/Plotting/plot_dth_both_tasks.m
+++ ANALYSIS_Full_experiment/DTH/Plotting/plot_dth_both_tasks.m
@@ -1,8 +1,9 @@
-function plot_dth_both_tasks(subjects,with_cross_task) %distance art, distance nat, RT
+function plot_dth_both_tasks(subjects,with_cross_task,with_stats) %distance art, distance nat, RT
 %PLOT_DTH_BOTH_TASKS Plot the fixed effects distance-to-hyperplane results for both
 %tasks.
 %
-%Input: subjects' ID (e.g., 1:13), add the cross task curves (1 yes, 0 no)
+%Input: subjects' ID (e.g., 1:13), add the cross task curves (1 yes, 0 no),
+%add stats (1 yes, 0 no)
 %
 %Correlates the decision values with reaction times (averaged over
 %participants) of each condition (60 scenes), at each timepoint, resulting in a plot of Spearman's correlation vs time. 
@@ -43,6 +44,37 @@ if with_cross_task
     plot(corr_both_cross_task_distraction_dist,'LineWidth',2)
     hold on;
 end
+
+%% Plot stats if needed
+if with_stats
+    for task = 1:2
+        task_name = get_task_name(task);
+        if task == 1
+            plot_location = -0.8;
+            color = 'b';
+        elseif task == 2
+            plot_location = -0.9;
+            color = 'm';
+        end
+   
+        %Check if stats already exist, otherwise run the stats script
+        filename = 'dth_permutation_stats';
+        filename_sign = fullfile(results_avg_dir,sprintf('%s_both_%s_distance_subjects_%d_%d',...
+            filename,task_name,subjects(1),subjects(end)));
+        if exist(filename_sign,'file')
+            load(filename_sign);
+            significant_timepoints = permutation_stats.SignificantMaxClusterWeight;
+        else
+            significant_timepoints = weighted_cluster_perm_stats(subjects,task_name,task_name,'both',1,10000);
+        end
+
+        %Plot the stats
+        st = (significant_timepoints*plot_location); %depending on the stats
+        st(st==0) = NaN;
+        plot(st,'*','Color',color); 
+        hold on;   
+    end
+end 
 
 %Plotting parameters
 plot_title =  sprintf('Correlation between the distance to hyperplane and reaction time in 60 scenes (N=%d)',numel(subjects));
