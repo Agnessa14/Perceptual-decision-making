5587147aaa41ae0c14c885013920bf3bb87d88c8
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-05-07 18:09:40 +0200

    changed to left tailed stats

--- ANALYSIS_Full_experiment/DTH/Plotting/plot_dth_both_tasks.m
+++ ANALYSIS_Full_experiment/DTH/Plotting/plot_dth_both_tasks.m
@@ -91,7 +91,7 @@ if with_stats
             load(filename_sign);
             significant_timepoints = permutation_stats.SignificantMaxClusterWeight;
         else
-            significant_timepoints = weighted_cluster_perm_stats(subjects,task,task,'both',1,num_perms);
+            significant_timepoints = weighted_cluster_perm_stats(subjects,task,task,'left',1,num_perms); %'left'->only look at the strength of negative correlations
         end
 
         %Plot the stats
