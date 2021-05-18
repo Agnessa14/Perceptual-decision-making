0f89cd68d3f4f3d9698c042d58d7ca3e94975d16
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-05-18 15:24:37 +0200

    added time generalization

--- ANALYSIS_Full_experiment/OTHER/RSA_cross_task.m
+++ ANALYSIS_Full_experiment/OTHER/RSA_cross_task.m
@@ -1,9 +1,11 @@
 function RSA_cross_task(subjects) 
-%RSA_CROSS_TASK Perform representational similarity analysis on the data from both tasks. 
+%RSA_CROSS_TASK Perform representational similarity analysis on SVM object decoding from both tasks. 
 %
 %Input: subject ID
 %
 %Output: 1xP vector of correlations in, where P is the number of timepoints. 
+%
+%Author: Agnessa Karapetian, 2021
 %
 
 %% Paths
@@ -16,56 +18,60 @@ numConditions = 60;
 numTimepoints = 200;
 sorted_subjects = sort(subjects);
 decoding_accuracies_all_subjects_cat = NaN(sorted_subjects(end),numConditions,numConditions,numTimepoints);
-decoding_accuracies_all_subjects_fix = NaN(sorted_subjects(end),numConditions,numConditions,numTimepoints);
-
+decoding_accuracies_all_subjects_dis = NaN(sorted_subjects(end),numConditions,numConditions,numTimepoints);
 
 %% Loop: collect results from all subjects 
 for subject = subjects
     subname = get_subject_name(subject);
     subject_results_dir = fullfile(results_dir,subname);
-    cat_filename = 'svm_decoding_accuracy_categorization.mat';
-    fix_filename = 'svm_decoding_accuracy_fixation.mat';
+    cat_filename = 'svm_decoding_accuracy_categorization.mat'; %rdm_pearson
+    dis_filename = 'svm_decoding_accuracy_fixation.mat'; %rdm_pearson
 
     load(fullfile(subject_results_dir,cat_filename));
     decoding_accuracies_all_subjects_cat(subject,:,:,:) = decodingAccuracy_avg;
-    load(fullfile(subject_results_dir,fix_filename));
-    decoding_accuracies_all_subjects_fix(subject,:,:,:) = decodingAccuracy_avg;
-    
+    load(fullfile(subject_results_dir,dis_filename));
+    decoding_accuracies_all_subjects_dis(subject,:,:,:) = decodingAccuracy_avg;
 end   
 
 %% RSA: correlate the RDMs from both tasks at every timepoint 
-average_DA_cat = squeeze(nanmean(decoding_accuracies_all_subjects_cat,1)); %avg over subjects
-average_DA_dis = squeeze(nanmean(decoding_accuracies_all_subjects_cat,1)); %avg over subjects
+rdm_cat = squeeze(nanmean(decoding_accuracies_all_subjects_cat,1)); %avg over subjects
+rdm_dis = squeeze(nanmean(decoding_accuracies_all_subjects_dis,1)); 
 
-%fill up the RDM so it's symmetrical
-t = 1:numTimepoints;
-% rdm_cat = NaN(size(average_DA_cat));
-% rdm_dis = NaN(size(average_DA_dis));
-rdm_cat = average_DA_cat;
-rdm_dis = average_DA_dis;
+% Fill up the RDM so it's symmetrical: replace the NaNs by 0s & add the transpose of the upper diagonal
+rdm_cat(isnan(rdm_cat)) = 0;
+rdm_dis(isnan(rdm_dis)) = 0;
 
-rdm_cat = reshape(rdm_cat(isnan(rdm_cat))==0,size(rdm_cat,1),size(rdm_cat,2),size(rdm_cat,3));
-rdm_dis = reshape(isnan(rdm_dis)==0,size(rdm_dis));
+rdm_cat_flattened_cell = arrayfun(@(x) squareform(rdm_cat(:,:,x)+(rdm_cat(:,:,x))'),...
+    1:numTimepoints,'UniformOutput',false);
+rdm_dis_flattened_cell = arrayfun(@(x) squareform(rdm_dis(:,:,x)+(rdm_dis(:,:,x))'),...
+    1:numTimepoints,'UniformOutput',false);
 
-rdm_cat = arrayfun(@(x) squareform(rdm_cat(:,:,x)+(rdm_cat(:,:,x))'), t);
-rdm_dis = arrayfun(@(x) squareform(rdm_dis(:,:,x)+(rdm_dis(:,:,x))'), t);
+rdm_cat_flattened = reshape(cell2mat(rdm_cat_flattened_cell),[],numTimepoints);
+rdm_dis_flattened = reshape(cell2mat(rdm_dis_flattened_cell),[],numTimepoints);
 
-% rdm_cat = arrayfun(@(x) squareform(average_DA_cat(:,:,x)),t);
-% rdm_dis = arrayfun(@(x) squareform(average_DA_dis(:,:,x)),t);
-rsa = arrayfun(@(x) corr(rdm_cat(t),rdm_dis(t),'type','Spearman'),t);
+% Perfom RSA at each combination of timepoints
+rsa = NaN(numTimepoints,numTimepoints);
+for tp1 = 1:numTimepoints
+    for tp2 = 1:numTimepoints
+        rsa(tp1,tp2) = 1-corr(rdm_cat_flattened(:,tp1),rdm_dis_flattened(:,tp2),'type','Spearman');
+    end
+end
 
 %% Plot
-figure(abs(round(randn*10))); %Random figure number
+h = pcolor(rsa); 
 set(gcf, 'Position', get(0, 'Screensize'));
-plot(rsa,'LineWidth',3);
-xlabel('Timepoint');
-ylabel('Spearman''s coefficient');
-title(sprintf('Representational similarity analysis of scene processing in categorization and distraction tasks (N=%d)',numel(subjects)));
+set(h, 'EdgeColor', 'none');
+axis square;
+cbar = colorbar;
+ylabel(cbar,'1-Spearman''s coefficient');
+xlabel('Timepoints: Distraction task');
+ylabel('Timepoints: Categorization task');
+title(sprintf('Time-generalized RSA of scene processing in categorization and distraction tasks (N=%d)',numel(subjects)));
 
 %% Save
 save(fullfile(results_avg_dir,sprintf('rsa_cross_task_subjects_%d_%d',subjects(1),subjects(end))),'rsa');
 saveas(gcf,fullfile(results_avg_dir,sprintf('rsa_cross_task_subjects_%d_%d',subjects(1),subjects(end)))); %save as matlab figure
-saveas(gcf,fullfile(results_avg_dir,sprintf('rsa_cross_task_subjects_%d_%d.svg',subjects(1),subjects(end)))); %save as svg
+saveas(gcf,fullfile(results_avg_dir,sprintf('rsa_cross_task_subjects_%d_%d.png',subjects(1),subjects(end)))); %save as png
 close(gcf);
 end
     
