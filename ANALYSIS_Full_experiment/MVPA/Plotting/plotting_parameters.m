4afc982988981d91265a33de77231c91c673699f
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-06-18 19:25:29 +0200

    changed the xticklabels

--- ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
+++ ANALYSIS_Full_experiment/MVPA/Plotting/plotting_parameters.m
@@ -1,11 +1,16 @@
-function plotting_parameters(plot_title,plot_legend,onset_time,font_size,legend_location,label_y)
-%PLOT_DATA Plot input data.
+function plotting_parameters(plot_title,title_bool,plot_legend,onset_time,font_size,legend_location,label_y)
+%PLOTTING_PARAMETERS Specify plot parameters.
 %
-%Input: title of the plot, legend of the plot, stimulus onset (in seconds)
+%Input: title of the plot, title bool (with or without title), legend of the plot, stimulus onset, font size,
+%legend location, label of the y axis
 %
+xticks(0:10:200);
+xticklabels(-200:50:800);
 xlim([0 201]);
-xlabel('Timepoints');
+xlabel('Time (ms)');
 ylabel(label_y);   
-title(plot_title);
+if title_bool
+    title(plot_title);
+end
 xline(onset_time,'--');
 legend(plot_legend,'Location',legend_location,'FontSize',font_size);
