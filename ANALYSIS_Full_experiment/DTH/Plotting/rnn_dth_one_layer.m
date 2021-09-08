aa9c4b49a5b12a08432b29c9038feb7dbbbbd608
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-09-08 19:19:22 +0200

    plotted all categories in one plot

--- ANALYSIS_Full_experiment/DTH/Plotting/rnn_dth_one_layer.m
+++ ANALYSIS_Full_experiment/DTH/Plotting/rnn_dth_one_layer.m
@@ -1,6 +1,10 @@
-function rnn_dth_one_layer
+function rnn_dth_one_layer(with_stats)
 %RNN_DTH_ONE_LAYER Performs distance-to-hyperplane  using
 %the distances-to-hyperplane using RNN dats. Plots the last layer.
+%
+%Input: with stats (1 or 0)
+%
+%Saves the plot and correlation values.
 %
 %Author: Agnessa Karapetian, 2021
 
@@ -8,6 +12,8 @@ function rnn_dth_one_layer
 addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment'));
 addpath(genpath('/scratch/agnek95/PDM/DATA/RNN_ACTIVATIONS'));
 addpath(genpath('/scratch/agnek95/PDM/DATA/RNN_RTs'));
+results_avg_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG';
+addpath(genpath(results_avg_dir));
 
 %% DTH
 numConditions = 60;
@@ -27,50 +33,102 @@ load('/scratch/agnek95/PDM/DATA/RNN_ACTIVATIONS/rnn_distances_all_scenes_model_2
 distances = data;
      
 %% Correlate each subject's distances with the median RT
-num_layers=size(distances,1);
+% num_layers=size(distances,1);
 num_timepoints_rnn=size(distances,2);
-size_corr = [num_layers,num_timepoints_rnn];
-correlation_art = NaN(size_corr);
-correlation_nat = NaN(size_corr);
-correlation_both = NaN(size_corr);
 all_conditions = [artificial_conditions natural_conditions];
 
-for l = 1:num_layers
-    for tp = 1:num_timepoints_rnn
-        correlation_art(l,:) = arrayfun(@(x) ...
-            corr(squeeze(distances(l,x,artificial_conditions)),...
-            RT_art,'type','Spearman'), 1:num_timepoints_rnn);
-        correlation_nat(l,:) = arrayfun(@(x) ...
-            corr(squeeze(distances(l,x,natural_conditions)),...
-            RT_nat,'type','Spearman'), 1:num_timepoints_rnn);
-        correlation_both(l,:) = arrayfun(@(x) ...
-            corr(squeeze(distances(l,x,all_conditions)),...
-            RT,'type','Spearman'), 1:num_timepoints_rnn);
+%only take the last layer
+for tp = 1:num_timepoints_rnn
+    correlation_art = arrayfun(@(x) ...
+        corr(squeeze(distances(end,x,artificial_conditions)),...
+        RT_art,'type','Spearman'), 1:num_timepoints_rnn);
+    correlation_nat = arrayfun(@(x) ...
+        corr(squeeze(distances(end,x,natural_conditions)),...
+        RT_nat,'type','Spearman'), 1:num_timepoints_rnn);
+    correlation_both = arrayfun(@(x) ...
+        corr(squeeze(distances(end,x,all_conditions)),...
+        RT,'type','Spearman'), 1:num_timepoints_rnn);
+end
+
+%% Plot
+figure(abs(round(randn*10)));
+cmap_1 = cool;
+cmap_2 = summer;
+color_art = cmap_1(200,:); %purple
+color_nat = cmap_2(100,:); %green
+color_both = 'k';
+plot(correlation_art,'LineWidth',2,'Color',color_art);
+hold on;
+plot(correlation_nat,'LineWidth',2,'Color',color_nat);
+plot(correlation_both,'LineWidth',2,'Color',color_both);
+
+%% Plot stats if needed
+if with_stats   
+    %define some variables for the stats and the plot
+    permutation_stats.num_perms = 1000;
+    permutation_stats.cluster_th = 0.05;
+    permutation_stats.significance_th = 0.05;
+    permutation_stats.tail = 'left';
+    for c = 1:3
+        if c == 1
+            category = 'artificial';            
+            plot_location = -0.7;
+            color = color_art;
+        elseif c == 2
+            category = 'natural';            
+            plot_location = -0.75;
+            color = color_nat;
+        elseif c == 3
+            category = 'both'; 
+            plot_location = -0.8;
+        end
+
+        %Check if stats already exist, otherwise run the stats script
+        filename_sign = 'rnn_layer7_dth_permutation_stats';   
+        filename = fullfile(results_avg_dir,sprintf('%s_%s.mat',filename_sign,category));
+        if exist(filename,'file')
+            load(filename,'permutation_stats');
+        else
+            [permutation_stats.SignificantMaxClusterWeight,permutation_stats.pValWeight,...
+                permutation_stats.SignificantMaxClusterSize,permutation_stats.pValSize] = ...
+                permutation_cluster_1sample_weight_alld(for_stats,permutation_stats.num_perms,...
+                permutation_stats.cluster_th,permutation_stats.significance_th,permutation_stats.tail); 
+        save(filename,'permutation_stats');
+        end
+        
+        %Plot
+        if c < 3
+            st = (permutation_stats.SignificantMaxClusterWeight*plot_location); %depending on the stats
+            st(st==0) = NaN;
+            plot(st,'*','Color',color); 
+            hold on;
+        end
     end
+end 
+
+%% Plotting parameters
+font_size = 18;
+set(gca,'FontName','Arial','FontSize',font_size);
+legend_plot = {'Artificial scenes','Natural scenes','All scenes'}; 
+% ylim([-0.5 0.3]);
+legend_bool = 0;
+title_bool = 0;
+plotting_parameters(plot_title,title_bool,legend_plot,legend_bool,font_size,'best','Spearman''s r'); 
+
+%% Save correlations and figures
+dth_results.corr_both_categories = avg_corr_both;
+dth_results.corr_artificial = avg_corr_art;
+dth_results.corr_natural = avg_corr_nat;
+
+save_path = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
+save(fullfile(save_path,'rnn_dth_layer7.mat'),'dth_results');
+saveas(gcf,fullfile(save_path,'rnn_dth_layer7.fig'));
+saveas(gcf,fullfile(save_path,'rnn_dth_layer7.svg'));
+
+close(gcf);
 end
 
 
-%% Surface plot
-for p = [1,3] %1:3
-    if p==1
-        conditions = 'all';
-        corr_plot = correlation_both;
-    elseif p==2
-        conditions = 'artificial';
-        corr_plot = correlation_art;
-    elseif p==3
-        conditions = 'natural';
-        corr_plot = correlation_nat;
-    end
-    surf(corr_plot);
-    colorbar;
-    title(sprintf('Correlation between RT and distances-to-the-hyperplane in an RNN (%s scenes)',conditions));
-    xlabel('Timepoint');
-    ylabel('Layer');
-    zlabel('Spearman''s r');
-    saveas(gcf,fullfile('SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/',sprintf('rnn_dth_surface_plot_%s.png',conditions)));
-    close(gcf);
-end
 
 
 
