46a9a59ad116abac12ceca855f00e91fee929a48
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-11-11 19:19:44 +0100

    added the cross-validation part

--- ANALYSIS_Full_experiment/OTHER/plot_RT_rnn_vs_human.m
+++ ANALYSIS_Full_experiment/OTHER/plot_RT_rnn_vs_human.m
@@ -14,22 +14,22 @@ results_avg_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
 
 %% Load RTs 
 numConditions = 60;
-entropy_thresh = '0.09';
+entropy_thresh = '0.07';
+model_name = 'model_02.11_2';
 
 %RNN
-load(sprintf('/scratch/agnek95/PDM/DATA/RNN_RTs/reaction_time_entropy_th_%s_model_22.08.mat',...
-    entropy_thresh),'data');
+load(sprintf('/scratch/agnek95/PDM/DATA/RNN_RTs/reaction_time_entropy_th_%s_%s.mat',...
+    entropy_thresh,model_name),'data');
 RT_rnn_all = data';
 
 %Human
-RT_human_all = NaN(max(subjects),numConditions);
+% RT_human_all = NaN(max(subjects),numConditions);
+load(fullfile(results_avg_dir,'RTs_shuffled_across_subjects.mat'),'data');
+RTs_eeg = data;
 
-for subject = subjects    
-    subname = get_subject_name(subject);
-    load(fullfile(results_dir,subname,'RTs_correct_trials_categorization.mat'),'RT_per_condition');
-    RT_human_all(subject,:) = RT_per_condition; 
-end
-RT_human_all_med = nanmedian(RT_human_all,1)';
+%Take second half of all subjects (shuffled) & get median
+num_subjects = numel(subjects);
+RT_human_all_med = nanmedian(RTs_eeg((num_subjects/2)+1:end,:),1)';
 
 
                         %%% Scatterplot: correlation rnn/human %%%
@@ -37,7 +37,6 @@ RT_human_all_med = nanmedian(RT_human_all,1)';
 corr_rt = corr(RT_human_all_med,RT_rnn_all,'type','Spearman');
 
 %% Plot
-num_subjects = numel(subjects);
 figure;
 color_plot = [0.05 0.6 0.13];
 scatter(normalize(RT_human_all_med),normalize(RT_rnn_all),[],color_plot,'filled');
@@ -54,6 +53,7 @@ if title_bool == 1
 end
 font_size = 18;
 set(gca,'FontName','Arial','FontSize',font_size);
+xlim([-3 3]);
 
 %% Stats
 if with_stats
@@ -62,7 +62,7 @@ if with_stats
     stats_behav.alpha = 0.05;
     stats_behav.tail = 'both';
     filename = fullfile(results_avg_dir,...
-        sprintf('stats_fdr_rnn_human_rt_correlation_subjects_%d_%d.mat',subjects(1),subjects(end)));
+        sprintf('cv_%s_stats_fdr_rnn_human_rt_correlation_subjects_%d_%d.mat',model_name,subjects(1),subjects(end)));
     if exist(filename,'file')
         load(filename,'stats_behav');
     else
@@ -80,9 +80,9 @@ if with_stats
 
     %check significance
     if stats_behav.pvalue < stats_behav.alpha
-        text(5,3,['R = ',num2str(corr_rt),', p = ',num2str(stats_behav.pvalue)],'FontSize',font_size); 
+        text(-3,1,['R = ',num2str(corr_rt),', p = ',num2str(stats_behav.pvalue)],'FontSize',font_size); 
     else
-        text(5,3,['R = ',num2str(corr_rt)],'FontSize',font_size); 
+        text(-3,1,['R = ',num2str(corr_rt)],'FontSize',font_size); 
     end
 
 end
@@ -92,8 +92,15 @@ if ~with_stats
 end
 
 %% Save
-saveas(gcf,fullfile(results_avg_dir,sprintf('rt_rnn_human_scatterplot_subjects_%d_%d',subjects(1),subjects(end))));
-saveas(gcf,fullfile(results_avg_dir,sprintf('rt_rnn_human_scatterplot_subjects_%d_%d.svg',subjects(1),subjects(end))));
+if strcmp(model_name,'model_02.11')
+    savename = '1';
+elseif strcmp(model_name,'model_02.11_2')
+    savename = '2';
+else
+    error('Unknown model name');
+end
+saveas(gcf,fullfile(results_avg_dir,sprintf('cv_%s_rt_rnn_human_scatterplot_subjects_%d_%d',savename,subjects(1),subjects(end))));
+saveas(gcf,fullfile(results_avg_dir,sprintf('cv_%s_rt_rnn_human_scatterplot_subjects_%d_%d.svg',savename,subjects(1),subjects(end))));
 close(gcf);
 
 end
