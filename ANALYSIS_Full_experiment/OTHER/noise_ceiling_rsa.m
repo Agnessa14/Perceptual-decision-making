d5e84e74ed1fe67a4bd1cb1829fbcf4d1dcc4aea
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-11-15 10:19:31 +0100

    added upper bound

--- ANALYSIS_Full_experiment/OTHER/noise_ceiling_rsa.m
+++ ANALYSIS_Full_experiment/OTHER/noise_ceiling_rsa.m
@@ -37,39 +37,70 @@ rdm_all_subjects = NaN(max(subjects),numConditions,numConditions,numTimepoints);
 for subject = subjects
     subname = get_subject_name(subject);
     subject_results_dir = fullfile(results_dir,subname);
-    load(fullfile(subject_results_dir,'split_within_task_rdm_pearson_categorization.mat'),'rdm_avg');           
-    rdm_all_subjects(subject,:,:,:) = squeeze(mean(rdm_avg(:,conds,conds,:),1)); %average over the RDMs of each half of trials      
+    load(fullfile(subject_results_dir,'rdm_pearson_categorization.mat'),'rdm_avg');           
+    rdm_all_subjects(subject,:,:,:) = rdm_avg(conds,conds,:); %average over the RDMs of each half of trials      
 end   
 
 %% Remove all subject nans 
 rdm_subjects = rdm_all_subjects(subjects,:,:,:);
-noise_ceiling = NaN(numSubjects,numTimepoints);
+lower_noise_ceiling = NaN(numSubjects,numTimepoints);
+upper_noise_ceiling = NaN(numSubjects,numTimepoints);
 
 %% Average over N-1 subjects and correlate with the left-out subject 
 for s = 1:numSubjects
     leftout_subject = squeeze(rdm_subjects(s,:,:,:));
     other_subjects = squeeze(nanmean(rdm_subjects(1:end~=s,:,:,:),1)); %avg over remaining subjects
-   
+    all_subjects = squeeze(nanmean(rdm_subjects,1));
+    
     %take upper triangular & flatten
     leftout_subject(isnan(leftout_subject)) = 0;
     other_subjects(isnan(other_subjects)) = 0;
+    all_subjects(isnan(all_subjects)) = 0;
     leftout_flattened_cell = arrayfun(@(x) squareform(leftout_subject(:,:,x)+(leftout_subject(:,:,x))'),...
         1:numTimepoints,'UniformOutput',false);
     leftout_flattened = reshape(cell2mat(leftout_flattened_cell),[],numTimepoints);
     other_flattened_cell = arrayfun(@(x) squareform(other_subjects(:,:,x)+(other_subjects(:,:,x))'),...
         1:numTimepoints,'UniformOutput',false);
     other_flattened = reshape(cell2mat(other_flattened_cell),[],numTimepoints);   
+    all_flattened_cell = arrayfun(@(x) squareform(all_subjects(:,:,x)+(all_subjects(:,:,x))'),...
+        1:numTimepoints,'UniformOutput',false);
+    all_flattened = reshape(cell2mat(all_flattened_cell),[],numTimepoints);   
+
+    %upper & lower bound ceilings
     for tp = 1:numTimepoints
-        noise_ceiling(s,tp) = corr(leftout_flattened(:,tp),other_flattened(:,tp));
+        lower_noise_ceiling(s,tp) = corr(leftout_flattened(:,tp),other_flattened(:,tp));
+        upper_noise_ceiling(s,tp) = corr(leftout_flattened(:,tp),all_flattened(:,tp));
     end
 end
 
 %% Average over iterations
-noise_ceiling_lower_bound = mean(noise_ceiling,1); %avg over subjects
-plot(noise_ceiling_lower_bound);
+noise_ceiling_lower_bound = mean(lower_noise_ceiling,1); %avg over subjects
+noise_ceiling_upper_bound = mean(upper_noise_ceiling,1); %avg over subjects
 
-% Save matrices
-save(fullfile(results_avg_dir,sprintf('noise_ceiling_rsa_subjects_%d_%d_%s_scenes',subjects(1),subjects(end),conditions)),'noise_ceiling_lower_bound');
+%% Plot
+plot(noise_ceiling_lower_bound,'DisplayName','Lower bound');
+legend('-DynamicLegend');
+hold all;
+plot(noise_ceiling_upper_bound,'DisplayName','Upper bound');
+xticks(0:20:200);
+xticklabels(-200:100:800);   
+ylim([-0.1 0.8]);
+plot_parameters = 1;
+if plot_parameters == 1
+%     title(sprintf('Upper and lower bound noise ceilings for %s scenes',conditions));
+    xlabel('Time (ms)');
+    ylabel('Spearman''s p');
+    font_size = 12;
+    set(gca,'FontName','Arial','FontSize',font_size);
+end
+filename = sprintf('noise_ceiling_rsa_subjects_%d_%d_%s_scenes',subjects(1),subjects(end),conditions);
+saveas(gcf,sprintf('%s.svg',filename));
+saveas(gcf,sprintf('%s.fig',filename));
+close(gcf);
+
+%Save matrices
+save(fullfile(results_avg_dir,sprintf('lower_%s',filename)),'noise_ceiling_lower_bound');
+save(fullfile(results_avg_dir,sprintf('upper_%s',filename)),'noise_ceiling_upper_bound');
 
 end
     
