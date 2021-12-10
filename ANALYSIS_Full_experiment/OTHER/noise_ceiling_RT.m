9b2a3bc2c8be099d48b80885a76d36ef3db64de6
Agnessa Karapetian <agnessakarapetian@gmail.com>
2021-12-10 14:18:04 +0100

    all subjects

--- ANALYSIS_Full_experiment/OTHER/noise_ceiling_RT.m
+++ ANALYSIS_Full_experiment/OTHER/noise_ceiling_RT.m
@@ -14,14 +14,13 @@ function noise_ceiling_RT(subjects)
 %
 
 %% Paths
-addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/'));
 results_avg_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
 
 %% Load the RTs
-num_subjects = numel(subjects)/2; %included subjects are only half (the rest were used for cross validation)
-filename = 'RTs_shuffled_across_subjects';
-load(fullfile(results_avg_dir,filename),'data');
-RTs_shuffled = data(num_subjects+1:end,:);
+num_subjects = numel(subjects); %included subjects are only half (the rest were used for cross validation)
+filename = 'RT_all_subjects_5_35_categorization.mat';
+load(fullfile(results_avg_dir,filename),'RTs');
+% RTs_shuffled = RTs(num_subjects+1:end,:);
 
 %% Average over N-1 subjects and correlate with the left-out subject 
 lower_noise_ceiling = NaN(num_subjects,3);
@@ -35,9 +34,12 @@ for s = 1:num_subjects
         elseif c == 3
             conds = 1:60;
         end
-        leftout_subject = squeeze(RTs_shuffled(s,conds));
-        other_subjects = squeeze(nanmean(RTs_shuffled(1:end~=s,conds),1)); %avg over remaining subjects
+        leftout_subject = squeeze(RTs(s,conds));
+        other_subjects = squeeze(nanmean(RTs(1:end~=s,conds),1)); %avg over remaining subjects
         nan_scenes = find(isnan(leftout_subject));
+        if s==6
+            keyboard;
+        end
         if numel(nan_scenes) > 0
             leftout_subject(nan_scenes) = [];
             other_subjects(nan_scenes) = [];
@@ -48,7 +50,7 @@ end
 
 %% Average over iterations and save
 noise_ceiling_lower_bound = mean(lower_noise_ceiling,1); %avg over subjects
-save(fullfile(results_avg_dir,sprintf('lower_bound_noise_ceiling_%s',filename)),'noise_ceiling_lower_bound');
+save(fullfile(results_avg_dir,sprintf('sf_lower_bound_noise_ceiling_%s',filename)),'noise_ceiling_lower_bound');
 
 end
     
