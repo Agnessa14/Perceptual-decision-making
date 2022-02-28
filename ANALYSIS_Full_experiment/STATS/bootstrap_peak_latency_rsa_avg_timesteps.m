62249865bdeb411a736ad47f713880728b2e3c68
Agnessa Karapetian <agnessakarapetian@gmail.com>
2022-02-28 18:59:03 +0100

    added median over timepoints

---
+++ ANALYSIS_Full_experiment/STATS/bootstrap_peak_latency_rsa_avg_timesteps.m
@@ -0,0 +1,188 @@
+function bootstrap_peak_latency_rsa_avg_timesteps(subjects,conditions_int)
+%BOOTSTRAP_PEAK_LATENCY_RSA_AVG_TIMESTEPS Apply bootstrapping to calculate the 95% confidence
+%interval of peak latency difference (for RSA between RNN and EEG-categorization task).
+%
+%Input: subject IDs (e.g., 1:13), conditions (1(all),2(artificial) or
+%3(natural)
+%
+%Output: saved boostrapped peak latencies (in ms), 95% confidence interval of the difference
+%
+%
+
+%% Paths
+addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/'));
+results_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS/';
+results_avg_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
+rnn_dir = '02.11_2_rnn/Input_RDM';
+
+                                    %%%%% SETUP %%%%%
+%% Load the subject-level RDMs
+if conditions_int==1
+    conds = 1:60;
+    conditions = 'all';
+elseif conditions_int==2
+    conds = 1:30;
+    conditions = 'artificial';
+elseif conditions_int==3
+    conds = 31:60;
+    conditions='natural';
+end
+
+numConditions = numel(conds);
+numTimepoints = 200;
+numTimepointsRNN = 8;
+layers_idx = [1,4,7]; %RNN layers of interest
+rdm_all_subjects = NaN(max(subjects),numConditions,numConditions,numTimepoints); 
+
+for subject = subjects
+    subname = get_subject_name(subject);
+    subject_results_dir = fullfile(results_dir,subname);
+    load(fullfile(subject_results_dir,'rdm_pearson_categorization.mat'),'rdm_avg');           
+    rdm_all_subjects(subject,:,:,:) = rdm_avg(conds,conds,:);      
+end   
+
+rdm_subjects = rdm_all_subjects(subjects,:,:,:); %remove NaN subjects
+
+%% Load the RNN RDMs & RSA results
+model_name = '02_11_2';
+filename_part = 'avg_tps_rsa_plus_noise_ceilings_subject_level_-100ms';
+filename = fullfile(results_avg_dir,sprintf('%s_%s_%s_subjects_%d_%d',...
+    model_name,filename_part,conditions,subjects(1),subjects(end)));
+load(fullfile(results_avg_dir,filename),'rsa_results');
+
+%Preallocate
+rdm_rnn = NaN(numel(layers_idx),numTimepointsRNN,numConditions,numConditions);
+peak_latency_ground_truth = NaN(numel(layers_idx),numTimepointsRNN);
+
+for layer = layers_idx
+    index_layer = find(layers_idx==layer);
+    %RDMs
+    for t = 1:numTimepointsRNN
+        load(fullfile(results_avg_dir,rnn_dir,sprintf('ReLU_Layer_%d_Time_%d_Input_RDM.mat',layer-1,t-1)),'data');
+            rdm_rnn = data(conds,conds);
+        for c1 = 1:numConditions
+            for c2 = 1:numConditions
+                if c1==c2
+                    rdm_rnn(index_layer,t,c1,c2) = 0; %Make sure the diagonal is all 0
+                end
+            end
+        end
+    end
+    %RSA results: get ground truth peak latency
+    rsa_results_l = squeeze(rsa_results(index_layer,:))';
+    peak_latency_ground_truth(index_layer) = (find(squeeze(rsa_results_l)==max(rsa_results_l))-40)*5;    
+end
+
+                                  %%%%% BOOTSTRAPPING %%%%%
+                                  
+%% 0) Prepare required variables
+num_bootstrap_samples = 1000;
+num_datasets = size(rdm_subjects,1);
+peak_latency = NaN(numel(layers_idx),numTimepointsRNN,num_bootstrap_samples);
+peak_latency_median_tps = NaN(numel(layers_idx),num_bootstrap_samples);
+max_dataset = num_datasets; %for the random dataset index generator
+min_dataset = 1;
+
+%% 1) Create the bootstrap samples & calculate the peak difference
+rng(0,'twister');
+
+%for each bootstrap sample, create a new dataset (N=30) and
+%calculate the peak latency
+for bs = 1:num_bootstrap_samples
+    rsa = NaN(num_datasets,numel(layers_idx),numTimepointsRNN,numTimepoints);   
+    for d = 1:num_datasets
+        idx = round((max_dataset-min_dataset).*rand(1,1) + min_dataset); %pick one random number between one and num_datasets
+        dataset = squeeze(rdm_subjects(idx,:,:,:));        
+        for layer = layers_idx
+            index_layer = find(layers_idx==layer);            
+            for t = 1:numTimepointsRNN                       
+                rsa(d,index_layer,t,:) = representational_SA_rnn(dataset,squeeze(rdm_rnn(index_layer,t,:,:)));
+            end
+        end
+    end
+    avg_rsa_datasets = squeeze(mean(rsa,1)); %avg over datasets
+    peak_latency(index_layer,t,bs) = (find(avg_rsa_datasets==max(avg_rsa_datasets),1)-40)*5;    
+    avg_rsa_tps = squeeze(median(avg_rsa_datasets,2));
+    peak_latency_median_tps(index_layer,bs) = (find(avg_rsa_tps==max(avg_rsa_tps),1)-40)*5;
+end
+
+%% 2) Calculate difference between intervals for bootstrap samples 
+%for each RNN timepoint 
+layer1_layer4_all_tps = abs(squeeze(peak_latency(1,:,:))-squeeze(peak_latency(2,:,:)));
+layer4_layer7_all_tps = abs(squeeze(peak_latency(2,:,:))-squeeze(peak_latency(3,:,:)));
+layer1_layer7_all_tps = abs(squeeze(peak_latency(1,:,:))-squeeze(peak_latency(3,:,:)));
+
+%for the median
+layer1_layer4_median_tps = abs(squeeze(peak_latency_median_tps(1,:))-squeeze(peak_latency_median_tps(2,:)));
+layer4_layer7_median_tps = abs(squeeze(peak_latency_median_tps(2,:))-squeeze(peak_latency_median_tps(3,:)));
+layer1_layer7_median_tps = abs(squeeze(peak_latency_median_tps(1,:))-squeeze(peak_latency(3,:)));
+
+%% 3) Get 95% confidence interval for peaks and for peak difference
+%all timepoints
+%peaks 
+CI_peaks_all_tps = NaN(numel(layers_idx),numTimepointsRNN,2);
+
+for layer = layers_idx
+    index_layer = find(layers_idx==layer);
+    for t = 1:numTimepointsRNN
+        CI_peaks_all_tps(index_layer,t,1) = prctile(squeeze(peak_latency(index_layer,t,:)),2.5);
+        CI_peaks_all_tps(index_layer,t,2) = prctile(squeeze(peak_latency(index_layer,t,:)),97.5);
+    end
+end
+
+%peak differences
+CI_diff_l1_l4_all_tps = NaN(numTimepointsRNN,2);
+CI_diff_l4_l7_all_tps = NaN(numTimepointsRNN,2);
+CI_diff_l1_l7_all_tps = NaN(numTimepointsRNN,2);
+
+for t = 1:numTimepointsRNN
+    CI_diff_l1_l4_all_tps(t,1) = prctile(squeeze(layer1_layer4_all_tps(t,:)),2.5);
+    CI_diff_l1_l4_all_tps(t,2) = prctile(squeeze(layer1_layer4_all_tps(t,:)),97.5);
+    CI_diff_l4_l7_all_tps(t,1) = prctile(squeeze(layer4_layer7_all_tps(t,:)),2.5);
+    CI_diff_l4_l7_all_tps(t,2) = prctile(squeeze(layer4_layer7_all_tps(t,:)),97.5);
+    CI_diff_l1_l7_all_tps(t,1) = prctile(squeeze(layer1_layer7_all_tps(t,:)),2.5);
+    CI_diff_l1_l7_all_tps(t,2) = prctile(squeeze(layer1_layer7_all_tps(t,:)),97.5);        
+end
+
+%median of timepoints
+%peaks
+CI_peaks_median_tps = NaN(numel(layers_idx),2);
+
+for layer = layers_idx
+    index_layer = find(layers_idx==layer);
+    CI_peaks_median_tps(index_layer,1) = prctile(squeeze(peak_latency_median_tps(index_layer,:)),2.5);
+    CI_peaks_median_tps(index_layer,2) = prctile(squeeze(peak_latency_median_tps(index_layer,:)),97.5);
+end
+
+%peak differences
+CI_diff_l1_l4_median_tps = NaN(1,2);
+CI_diff_l4_l7_median_tps = NaN(1,2);
+CI_diff_l1_l7_median_tps = NaN(1,2);
+
+CI_diff_l1_l4_median_tps(1) = prctile(layer1_layer4_median_tps,2.5);
+CI_diff_l1_l4_median_tps(2) = prctile(layer1_layer4_median_tps,97.5);
+CI_diff_l4_l7_median_tps(1) = prctile(layer4_layer7_median_tps,2.5);
+CI_diff_l4_l7_median_tps(2) = prctile(layer4_layer7_median_tps,97.5);
+CI_diff_l1_l7_median_tps(1) = prctile(layer1_layer7_median_tps,2.5);
+CI_diff_l1_l7_median_tps(2) = prctile(layer1_layer7_median_tps,97.5);        
+
+
+%% Save as structure
+bootstrap_peak_latencies_rsa.peak_latency_bs_all_tps = squeeze(mean(peak_latency,3));
+bootstrap_peak_latencies_rsa.peak_latency_bs_median_tps = squeeze(mean(peak_latency_median_tps,2));
+bootstrap_peak_latencies_rsa.peak_latency_true_all_tps = peak_latency_ground_truth;
+bootstrap_peak_latencies_rsa.peak_latency_true_median_tps = median(peak_latency_ground_truth,2);
+
+bootstrap_peak_latencies_rsa.CI_peaks_all_tps = CI_peaks_all_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l1_l4_all_tps = CI_diff_l1_l4_all_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l4_l7_all_tps = CI_diff_l4_l7_all_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l1_l7_all_ps = CI_diff_l1_l7_all_tps;
+
+bootstrap_peak_latencies_rsa.CI_peaks_median_tps = CI_peaks_median_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l1_l4_median_tps = CI_diff_l1_l4_median_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l4_l7_median_tps = CI_diff_l4_l7_median_tps;
+bootstrap_peak_latencies_rsa.CI_diff_l1_l7_median_ps = CI_diff_l1_l7_median_tps;
+
+save(fullfile(results_avg_dir,sprintf('bootstrap_peak_latencies_rsa_all_and_median_tps_%s_subjects_%d_%d',conditions,subjects(1),subjects(end))),'bootstrap_peak_latencies_rsa');
+
+end
