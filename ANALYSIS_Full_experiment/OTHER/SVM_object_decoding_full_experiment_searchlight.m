b8eb698eabc7b6c4dfe368ba5a3e734ad3849292
Agnessa Karapetian <agnessakarapetian@gmail.com>
2022-05-09 12:04:32 +0200

    initial commit

---
+++ ANALYSIS_Full_experiment/OTHER/SVM_object_decoding_full_experiment_searchlight.m
@@ -0,0 +1,109 @@
+function SVM_object_decoding_full_experiment_searchlight(subject,task)
+%SVM_OBJECT_DECODING_FULL_EXPERIMENT Perform object decoding (average of pairwise object decoding) using the SVM classifier.
+%
+%Input: subject ID, task (1=categorization, 2=fixation)
+%
+%Output: NxNxP vector of accuracies in %, where N is the number of conditions and
+%P is the number of timepoints.
+%
+%Author: Agnessa Karapetian (2021), modified by Muthukumar Pandaram (2022)
+
+%% Set-up prereqs
+%add paths
+addpath(genpath('/scratch/agnek95/PDM/DATA/DATA_FULL_EXPERIMENT'));
+addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/'));
+addpath('/home/agnek95/OR/TOOLBOX/MVNN/MEG_SVM_decoding_MVNN'); %MVNN toolbox
+addpath(genpath('/home/agnek95/OR/ANALYSIS/DECODING/libsvm')); %libsvm toolbox
+addpath('/home/agnek95/OR/ANALYSIS/DECODING'); %MVNN function
+addpath('/home/agnek95/OR/TOOLBOX/fieldtrip-20190224');
+ft_defaults;
+
+% load neighbourhood map
+load('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/OTHER/EEG_neighbourhoods.mat')
+
+%subject and task name strings
+subname = get_subject_name(subject);
+task_name = get_task_name(task);
+
+%check if there's a directory for that subject, otherwise create one
+results_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS';
+
+if ~isfolder(fullfile(results_dir,subname))
+    mkdir(results_dir,subname);
+end
+
+%% Prepare data
+%load eeg and behavioural data
+data_dir = fullfile('/scratch/agnek95/PDM/DATA/DATA_FULL_EXPERIMENT/',subname);
+load(fullfile(data_dir,sprintf('timelock_%s',task_name))); %eeg
+load(fullfile(data_dir,sprintf('preprocessed_behavioural_data_%s',task_name)));
+
+%only keep the trials with a positive RT & correct response
+timelock_triggers = timelock.trialinfo(behav.RT>0 & behav.points==1); %triggers
+timelock_data = timelock.trial(behav.RT>0 & behav.points==1,:,:); %actual data
+
+%% Define the required variables
+numConditions = 60;
+[numTrials, ~] = min_number_trials(timelock_triggers, numConditions); %minimum number of trials per scene
+numTimepoints = size(timelock_data,3); %number of timepoints
+numPermutations=100;
+numChannels = size(timelock_data,2);
+
+%Preallocate
+decodingAccuracy=NaN(numPermutations,numConditions,numConditions,numTimepoints,numChannels);
+chanIdx = 1:numChannels; % select all channels
+
+%% Decoding
+for perm = 1:numPermutations
+    tic
+    disp('Creating the data matrix');
+    data = create_data_matrix(numConditions,timelock_triggers,numTrials,timelock_data);
+    
+    disp('Performing MVNN');
+    data = multivariate_noise_normalization(data);
+    
+    disp('Binning data into pseudotrials');
+    numTrialsPerBin = round(numTrials/6);
+    [pseudoTrials,numPTs] = create_pseudotrials(numTrialsPerBin,data);
+    
+    % Modifing neighbourhood and pseudotrial matrix due to missing channels
+    [neighbourhoods, missing_channel_ids, pseudoTrials] = correct_neighbourhood_map(timelock.label,pseudoTrials);
+    
+    %only get the upper diagonal
+    for condA=1:numConditions-1 %1:59
+        for condB = condA+1:numConditions %2:60
+            for timePoint = 1:numTimepoints
+                disp(['Running the classification: 1st sample ->', num2str(condA), ', 2nd sample ->',num2str(condB),...
+                    ', timepoint ->',num2str(timePoint)]);
+                for iChan = chanIdx
+                    if ~ismember(iChan,missing_channel_ids) || isempty(missing_channel_ids)                        
+                        neighbours = neighbourhoods(iChan , :);
+                        neighbours = neighbours(~isnan(neighbours));
+                        
+                        % L-1 pseudo trials go to training set, the Lth to testing set
+                        training_data=[squeeze(pseudoTrials(condA,1:end-1,neighbours,timePoint)) ; squeeze(pseudoTrials(condB,1:end-1,neighbours,timePoint))]; %(numbins-1)xnum_channelsx1 each
+                        testing_data=[squeeze(pseudoTrials(condA,end,neighbours,timePoint))' ; squeeze(pseudoTrials(condB,end,neighbours,timePoint))']; %1x63x1 each
+                        
+                        % class labels
+                        labels_train=[ones(1,numPTs-1) 2*ones(1,numPTs-1)]; %one label for each pseudotrial
+                        labels_test= [1 2];
+                        
+                        train_param_str= '-s 0 -t 0 -b 0 -c 1 -q'; %look up the parameters online if needed
+                        model=svmtrain01(labels_train',training_data,train_param_str);
+                        [~, accuracy, ~,] = svmpredict(labels_test',testing_data,model);
+                        decodingAccuracy(perm,condA,condB,timePoint,iChan)=accuracy(1);
+                    end
+                end
+            end
+        end
+    end
+    toc
+end
+
+%% Save the decoding accuracies
+decodingAccuracy_avg = squeeze(mean(decodingAccuracy,1)); %average over permutations
+save(fullfile(results_dir,subname,sprintf('svm_searchlight_object_decoding_accuracy_%s.mat',task_name)),'decodingAccuracy_avg');    
+
+end
+
+
---
+++ ANALYSIS_Full_experiment/OTHER/correct_neighbourhood_map.m
@@ -0,0 +1,62 @@
+function [neighbourhood_map, all_missing_channel_ids, data_mod] = correct_neighbourhood_map(channels_labels_in_data, data)
+% This function is used to correct the neighbourhood matrix for grouping
+% the electrodes with nearest k-neighbours
+%
+%
+% Input: channels_labels_in_data : The channels in the data (Array of
+%                                  channel names with size Num channels x 1)
+%        data : The EEG data (num_conditions x num_conditions x num_channels x num_timepoints)
+%
+% Output: neighbourhood_map : A matrix (double) with a dimension of number
+%                             of channels x k (number of neighbours to be
+%                             grouped)
+%         all_missing_channel_ids : An array with the indexes of the
+%                                   missing channels in the neighbourhoods
+%                                   file containing all channels returns
+%                                   empty if there are no missing channels
+%         data_mod : The modified data matrix with the missing channel data
+%                added as NaN at the missing channel ids.
+% Author: Muthukumar Pandaram
+
+
+addpath('D:\MATLAB_packages\fieldtrip-release');
+ft_defaults;
+
+% Load channel locations
+load('D:\Lab rotation\Neural dynamics of Visual Cognition\Project_files\Searchlight\adult_63channels.mat')
+all_channel_labels = extractfield(channelloc,'labels')';
+
+% Check for missing channels
+channels_missing = setdiff(all_channel_labels, channels_labels_in_data);
+% disp('The missing channels are: ', channels_missing);
+
+% load Monika's neighbourhood (neighbourhoods, double)
+load('D:\Lab rotation\Neural dynamics of Visual Cognition\Project_files\Searchlight\EEG_neighbourhoods.mat')
+neighbourhood_map = neighbourhoods;
+
+%Find the number id for the missing channel labels and remove them from the
+%neighbourhoods map
+
+all_missing_channel_ids = double.empty;
+data_mod = data;
+
+for i = 1:length(channels_missing)
+    all_missing_channel_ids(i,1) = find(contains(all_channel_labels,channels_missing(i)));
+end
+ 
+all_missing_channel_ids = sort(all_missing_channel_ids);
+
+for i = 1:length(channels_missing)
+    for j = 1:size(neighbourhood_map,1)
+        ind=neighbourhood_map(j,:)== all_missing_channel_ids(i,1);
+        neighbourhood_map(j, ind) = NaN;        
+    end
+    substitute_missing_data = NaN(size(data,1),size(data,2),1,size(data,4));
+    data_mod = cat(3,data_mod(:,:,1:all_missing_channel_ids(i,1)-1,:),substitute_missing_data,data_mod(:,:,all_missing_channel_ids(i,1):end,:));
+end
+
+if ~isempty(channels_missing)
+    neighbourhood_map(all_missing_channel_ids,:) = NaN;    
+end
+
+end
