2239b42a7f7e29626413b44f1ef22cc939625ae5
Agnessa Karapetian <agnessakarapetian@gmail.com>
2022-05-11 12:45:21 +0200

    added code to check if file exists

---
+++ ANALYSIS_Full_experiment/MVPA/Average/all_subjects_decoding_sl.m
@@ -0,0 +1,62 @@
+function all_subjects_decoding_sl(subjects,analysis,task)
+%ALL_SUBJECTS_DECODING_SL (categorization and distraction). Collects decoding the
+%data from subjects to put it in one matrix.
+%
+%Input: subject IDs, analysis ('svm_object_decoding', 'pearson_object_decoding' or 'category_decoding'),
+%task (1 or 2)
+%Output: matrix containing decoding accuracies for multiple subjects,
+%timepoints and channels
+%
+%Author: Agnessa Karapetian, 2021
+%
+
+%% Paths
+addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/'));
+results_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS/';
+results_avg_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
+
+%% Preallocate
+numConditions = 60;
+numTimepoints = 10;
+numChannels = 63;
+if strcmp(analysis,'object_decoding')
+    decoding_accuracies_all_subjects = NaN(max(subjects),numConditions,numConditions,numTimepoints,numChannels);
+elseif strcmp(analysis,'category_decoding')
+    decoding_accuracies_all_subjects = NaN(max(subjects),numTimepoints,numChannels);
+end
+task_name = get_task_name(task);
+
+%% Loop: collect results from all subjects
+for subject = subjects
+    subname = get_subject_name(subject);
+    subject_results_dir = fullfile(results_dir,subname);
+    if strcmp(analysis,'svm_object_decoding')
+        filename = sprintf('svm_searchlight_object_decoding_accuracy_%s.mat',task_name);
+        all_dimensions = repmat({':'},1,4); %conditions x conditions x timepoints x channels
+    elseif strcmp(analysis,'pearson_object_decoding')
+        filename = sprintf('rdm_pearson_searchlight_%s.mat',task_name);
+        all_dimensions = repmat({':'},1,4); %conditions x conditions x timepoints x channels        
+    elseif strcmp(analysis,'category_decoding')
+        filename = sprintf('cross_validated_dth_pseudotrials_svm_decodingAccuracy_searchlight_%s.mat',task_name);
+        all_dimensions = repmat({':'},1,2); % timepoints x channels 
+    end
+    filename_full = fullfile(subject_results_dir,filename);
+    if ~exist(filename_full,'file')
+        disp(subject);
+    else
+        if strcmp(analysis,'svm_object_decoding') || strcmp(analysis,'category_decoding')
+            load(filename_full,'decodingAccuracy_avg');
+            decoding_accuracies_all_subjects(subject,all_dimensions{:}) = decodingAccuracy_avg;
+        elseif strcmp(analysis,'pearson_object_decoding')
+            load(filename_full,'rdm_avg');
+            decoding_accuracies_all_subjects(subject,all_dimensions{:}) = rdm_avg;
+        end
+    end
+end
+
+decoding_accuracies = decoding_accuracies_all_subjects(subjects,all_dimensions{:});
+
+%% Save the matrix
+save(fullfile(results_avg_dir,sprintf('%s_all_subjects_%s.mat',analysis,task_name)),'decoding_accuracies'); 
+
+end
