deea9618365728fb9dda9dd1af727fad12cf3a75
Agnessa Karapetian <agnessakarapetian@gmail.com>
2022-05-17 18:50:10 +0200

    fixed indexing

--- ANALYSIS_Full_experiment/MVPA/Average/all_subjects_decoding_sl.m
+++ ANALYSIS_Full_experiment/MVPA/Average/all_subjects_decoding_sl.m
@@ -24,7 +24,7 @@ if strcmp(analysis,'object_decoding')
     decoding_accuracies_all_subjects = NaN(max(subjects),numConditions,numConditions,numTimepoints,numChannels);
 elseif strcmp(analysis,'category_decoding')
     decoding_accuracies_all_subjects = NaN(max(subjects),numTimepoints,numChannels);
-    distances_all_subjects = NaN(max(subjects),numTimepointsDTH,numChannels);
+    distances_all_subjects = NaN(max(subjects),numConditions,numTimepointsDTH,numChannels);
 end
 task_name = get_task_name(task);
 
@@ -40,7 +40,7 @@ for subject = subjects
         all_dimensions = repmat({':'},1,4); %conditions x conditions x timepoints x channels        
     elseif strcmp(analysis,'category_decoding')
         filename = sprintf('cross_validated_dth_pseudotrials_svm_decodingAccuracy_searchlight_%s.mat',task_name);
-        all_dimensions = repmat({':'},1,2); % timepoints x channels 
+        all_dimensions = repmat({':'},1,3); % timepoints x channels 
     end
     filename_full = fullfile(subject_results_dir,filename);
     if ~exist(filename_full,'file')
@@ -49,12 +49,12 @@ for subject = subjects
         if strcmp(analysis,'svm_object_decoding') || strcmp(analysis,'category_decoding')
             load(filename_full,'decodingAccuracy_avg');
             decoding_accuracies_all_subjects(subject,all_dimensions{:}) = decodingAccuracy_avg;
-        elseif strcmp(analysis,'pearson_object_decoding')
-            load(filename_full,'rdm_avg');
-            decoding_accuracies_all_subjects(subject,all_dimensions{:}) = rdm_avg;
             filename_dist = sprintf('cross_validated_dth_pseudotrials_svm_decisionValues_searchlight_all_timepoints_%s.mat',task_name);
             load(fullfile(subject_results_dir,filename_dist),'decisionValues_Avg');
             distances_all_subjects(subject,all_dimensions{:}) = decisionValues_Avg;
+        elseif strcmp(analysis,'pearson_object_decoding')
+            load(filename_full,'rdm_avg');
+            decoding_accuracies_all_subjects(subject,all_dimensions{:}) = rdm_avg;
         end
     end
 end
@@ -62,9 +62,10 @@ end
 decoding_accuracies = decoding_accuracies_all_subjects(subjects,all_dimensions{:});
 
 %% Save the matrix
-save(fullfile(results_avg_dir,sprintf('%s_all_subjects_searchlight_%s.mat',analysis,task_name)),'decoding_accuracies'); 
 if strcmp(analysis,'category_decoding')
     distances = distances_all_subjects(subjects,all_dimensions{:});
-    save(fullfile(results_avg_dir,sprintf('%distances_all_subjects_all_timepoints_searchlight_%s.mat',task_name)),'distances'); 
-end
+    save(fullfile(results_avg_dir,sprintf('distances_all_subjects_all_timepoints_searchlight_%s.mat',task_name)),'distances'); 
+    save(fullfile(results_avg_dir,sprintf('%s_all_subjects_all_timepoints_searchlight_%s.mat',analysis,task_name)),'decoding_accuracies'); 
+else
+    save(fullfile(results_avg_dir,sprintf('%s_all_subjects_searchlight_%s.mat',analysis,task_name)),'decoding_accuracies'); 
 end
