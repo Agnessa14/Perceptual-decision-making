bd3b30ccdefa1f01a8e60ddd9b5a25d2bc2bde6c
Agnessa Karapetian <agnessakarapetian@gmail.com>
2022-09-13 17:46:51 +0200

    fixed filename commit

--- ANALYSIS_Full_experiment/OTHER/bootstrap_peak_latency_dth.m
+++ ANALYSIS_Full_experiment/OTHER/bootstrap_peak_latency_dth.m
@@ -9,7 +9,6 @@ function bootstrap_peak_latency_dth(task_distance,task_RT)
 
 %% Paths
 addpath(genpath('/home/agnek95/SMST/PDM_PILOT_2/ANALYSIS_Full_experiment/'));
-% results_dir = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS/';
 results_avg = '/home/agnek95/SMST/PDM_FULL_EXPERIMENT/RESULTS_AVG/';
                                     %%%%% SETUP %%%%%
 %% Load all subjects' data
@@ -19,48 +18,59 @@ for c = 1:3 %artificial, natural, both
     switch c
         case 1
             conditions = 'artificial';
+            conds_short = conditions(1:3);
         case 2 
             conditions = 'natural';
+            conds_short = conditions(1:3);
         case 3
             conditions = 'both';
+            conds_short = conditions;
     end
-    filename = sprintf('all_subjects_dth_%s',task_name_distance,conditions);
+    filename = sprintf('all_subjects_dth_%s_%s',task_name_distance,conditions);
     if task_distance~=task_RT
-        filename = sprintf('all_subjects_dth_%s_distance_%s',task_name_distance,conditions);
+        filename = sprintf('all_subjects_dth_%s_distances_%s',task_name_distance,conditions);
     end
     load(fullfile(results_avg,filename));
-    
-end
+    all_sub_dth = eval(sprintf('all_sub_dth_%s',conds_short));
+     
+    %%%%% BOOTSTRAPPING %%%%%
 
-                                  %%%%% BOOTSTRAPPING %%%%%
+    %% 0) Prepare required variables
+    num_bootstrap_samples = 1000;
+    num_datasets = size(all_sub_dth,1); %num subjects
+    peak_latency_all_samples = NaN(num_bootstrap_samples,1);
 
-%% 0) Prepare required variables
-num_bootstrap_samples = 1000;
-num_datasets = size(decoding_accuracies_all,1);
-peak_latency_all_samples = NaN(num_bootstrap_samples,1);
-
-%% 1) Create the bootstrap samples
-rng(0,'twister');
-max_dataset = num_datasets; %for the random dataset index generator
-min_dataset = 1;
-for bs = 1:num_bootstrap_samples
-    datasets = NaN(size(decoding_accuracies_all));
-    for d = 1:num_datasets
-        idx = round((max_dataset-min_dataset).*rand(1,1) + min_dataset); %pick one random number between one and num_datasets
-        datasets(d,:) = decoding_accuracies_all(idx,:);
+    %% 1) Create the bootstrap samples
+    rng(0,'twister');
+    max_dataset = num_datasets; %for the random dataset index generator
+    min_dataset = 1;
+    for bs = 1:num_bootstrap_samples
+        datasets = NaN(size(all_sub_dth));
+        for d = 1:num_datasets
+            idx = round((max_dataset-min_dataset).*rand(1,1) + min_dataset); %pick one random number between one and num_datasets
+            datasets(d,:) = all_sub_dth(idx,:);
+        end
+        avg_datasets = squeeze(mean(datasets,1));
+        peak_latency_all_samples(bs) = find(avg_datasets==min(avg_datasets),1);
     end
-    avg_datasets = squeeze(mean(datasets,1));
-    peak_latency_all_samples(bs) = find(avg_datasets==max(avg_datasets),1);
+
+    %% 2) Get ground mean bootstrapped peak latency & max value *actually min bcs we are interested in negative correlations*
+    mean_all_sub_dth = mean(all_sub_dth,1);
+    bootstrap_dth.peak_value_ground = min(mean_all_sub_dth);
+    bootstrap_dth.peak_latency_ground = (find(mean_all_sub_dth==min(mean_all_sub_dth))-40)*5;
+    
+    %% 3) Get 95% confidence interval
+    bootstrap_dth.confidence_interval = NaN(2,1);
+    bootstrap_dth.confidence_interval(1) = (prctile(peak_latency_all_samples,2.5)-40)*5;
+    bootstrap_dth.confidence_interval(2) = (prctile(peak_latency_all_samples,97.5)-40)*5;
+    fprintf('Peak value: %d \nPeak latency: %d \nConfidence interval: [%d %d]\n',...
+        bootstrap_dth.peak_value_ground,bootstrap_dth.peak_latency_ground,bootstrap_dth.confidence_interval(1),bootstrap_dth.confidence_interval(2)); 
+    %% Save
+    save(fullfile(results_avg,sprintf('bootstrap_%s',filename')),'bootstrap_dth');
 end
 
-%% 2) Get mean bootstrapped peak latency
-peak_latency = round(mean(peak_latency_all_samples));
 
-%% 3) Get 95% confidence interval
-confidence_interval = NaN(2,1);
-confidence_interval(1) = prctile(peak_latency_all_samples,2.5);
-confidence_interval(2) = prctile(peak_latency_all_samples,97.5);
-
+end
 
 
 
