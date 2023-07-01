18f40158ee5baf3a42558313e1c407eb0e059905
Agnessa Karapetian <agnessakarapetian@gmail.com>
2023-07-01 18:40:19 +0200

    added bd net readout models

--- ANALYSIS_Full_experiment/DNN/rcnn_sat/__init__.py
+++ ANALYSIS_Full_experiment/DNN/rcnn_sat/__init__.py
@@ -1,3 +1,4 @@
 from .preprocess import preprocess_image
 from .b_net import b_net, b_k_net, b_k_net, b_d_net
 from .bl_net import bl_net
+from .b_d_net_readouts import b_d_net_readouts, b_d_net_readout_0, b_d_net_readout_1, b_d_net_readout_2, b_d_net_readout_3, b_d_net_readout_4, b_d_net_readout_5, b_d_net_readout_final
